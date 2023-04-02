f947cf8f1acaa28a9dadda4b677f0c2cecb83c52
x4dr <x4dr@hotmail.com>
2023-04-02 22:54:08 +0200

    delete Games.py and homebrew Button, remove vestigial hikari references

--- Golconda/Rights.py
+++ Golconda/Rights.py
@@ -2,7 +2,6 @@ import logging
 from typing import Callable, TypeVar, ParamSpec, Awaitable
 
 import discord
-import hikari
 from gamepack.Dice import DescriptiveError
 
 from Golconda.Storage import evilsingleton, Storage
@@ -39,18 +38,18 @@ async def allowed(msg: discord.Message) -> bool:
     return (msg.content or "").strip().lower().startswith(s.me.name.lower())
 
 
-def is_owner(u: hikari.User):
+def is_owner(u: discord.User):
     return s.client.application.owner == u
 
 
 def owner_only(f: Callable[P, Awaitable[T]]) -> Callable[P, Awaitable[T]]:
-    def inner(calling_user: hikari.User, *args, **kwargs):
+    def inner(calling_user: discord.User, *args, **kwargs):
         if is_owner(calling_user):
             kwargs["user"] = kwargs.get("user") or calling_user.id
             # replace user arg by calling user only if it's not given, that way, admin can call for another
             return f(calling_user, *args, **kwargs)
         else:
-            logger.info(f"tried to access {f.__name__} as {calling_user.username}")
+            logger.info(f"tried to access {f.__name__} as {calling_user.name}")
 
             # noinspection PyUnusedLocal
             async def dummy(*a, **b):
