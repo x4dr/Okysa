deb1e63231629704df46b4f4e9b8525523a1cc16
x4dr <x4dr@hotmail.com>
2023-06-05 15:20:12 +0200

    call soundpipe setup script before opening soundpipe

--- Commands/Voice.py
+++ Commands/Voice.py
@@ -7,6 +7,7 @@ def soundpipe():
     # #!/bin/bash
     # pacat -r -d alsa_output.[input] \
     # --format=s16le --rate=48000 -ac 2 > ~/soundpipe
+    os.system("setup_soundpipe")
     return discord.FFmpegPCMAudio(
         os.path.expanduser("~/soundpipe"),
         before_options="-f s16le -ar 48000 -ac 2",
@@ -26,15 +27,3 @@ def register(tree: discord.app_commands.CommandTree):
         finally:
             await connection.disconnect()
             await interaction.delete_original_response()
-
-    @tree.command(name="sync")
-    async def sync(interaction: discord.Interaction):
-        vp = interaction.guild.voice_client
-        if not vp or not isinstance(vp, discord.VoiceClient) or not vp.is_connected():
-            # noinspection PyUnresolvedReferences
-            await interaction.response.send_message("Not connected", ephemeral=True)
-            return
-        vp.stop()
-        vp.play(soundpipe())
-        # noinspection PyUnresolvedReferences
-        await interaction.response.send_message("Synced", ephemeral=True)
