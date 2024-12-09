36e1f19f1242d846c2fedc406ebbec2896d83bee
x4dr <x4dr@hotmail.com>
2024-12-10 00:32:04 +0100

    more eastereggs

--- Golconda/eastereggs.py
+++ Golconda/eastereggs.py
@@ -15,6 +15,21 @@ praise_phrases = [
     r"i love you",
 ]
 
+hate_phrases = [
+    r"fuck(?: you| off)?",
+    r"i hate(?: you| it)?",
+    r"you're (?:awful|terrible|horrible|the worst)",
+    r"(?:stupid|dumb|idiot|moron)",
+    r"go away",
+    r"shut up",
+    r"nobody likes you",
+    r"why are you here",
+    r"useless bot",
+    r"(?:kill|delete) yourself",
+    r"(go )?kys",
+    r"(go )?fuck you(rself)?",
+]
+
 
 async def eastereggs(message: discord.Message):
     if message.guild:
@@ -26,6 +41,12 @@ async def eastereggs(message: discord.Message):
     praise_pattern = re.compile(
         rf"(?:{'|'.join(praise_phrases)})[, ]+{re.escape(bot_name)}", re.IGNORECASE
     )
+    hate_pattern = re.compile(
+        rf"(?:{'|'.join(hate_phrases)})[, ]*{re.escape(bot_name)}?", re.IGNORECASE
+    )
 
     if praise_pattern.search(message.content):
         await message.add_reaction("😳")
+    elif hate_pattern.search(message.content):
+        await message.add_reaction("🖕")
+        await message.add_reaction("😭")
